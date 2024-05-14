128490e6559ff113eba1e562d8c9ed3d0bc736d9
ryankwan7 <59375660+ryankwan7@users.noreply.github.com>
2024-05-13 17:33:22 -0700

    added assertations to test file

--- Two Sum/main.py
+++ Two Sum/main.py
@@ -1,6 +1,8 @@
 import two_sum
 
-def test(nums: list, target: int):
+def test(nums: list, target: int, output: list):
+    assert two_sum.two_sum(nums, target) == output
+    assert two_sum.two_sum_2(nums, target) == output
     print("Input: nums = " + str(nums) + ", target = " + str(target))
     print("Output (simple): " + str(two_sum.two_sum(nums, target)))
     print("Output (hash table): " + str(two_sum.two_sum_2(nums, target)) + "\n")
@@ -8,15 +10,18 @@ def test(nums: list, target: int):
 def main():
     nums = [2, 7, 11, 15]
     target = 9
-    test(nums, target)
+    output = [0, 1]
+    test(nums, target, output)
 
     nums = [3, 2, 4]
     target = 6
-    test(nums, target)
+    output = [1, 2]
+    test(nums, target, output)
 
     nums = [3, 3]
     target = 6
-    test(nums, target)
+    output = [0, 1]
+    test(nums, target, output)
 
 if __name__ == "__main__":
     main()
